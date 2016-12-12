2585cfdc064841a41ed597ce431412d4bb9d9875
Dan Delany <dand@spotify.com>
2016-12-11 22:00:04 -0500

    add test node client

---
+++ node-client/index.js
@@ -0,0 +1,10 @@
+const socketCluster = require('socketcluster-client');
+
+const socket = socketCluster.connect({hostname: 'Dans-MacBook-Pro-2.local', port: '8000'});
+
+socket.on('connect', (data, res) => {
+  console.log('connected', data, res);
+});
+
+socket.emit('sampleClientEvent', {message: 'Event from Jerry Tank!'});
+
