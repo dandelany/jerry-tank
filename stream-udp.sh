b9bc42d03f8b8badf736edada215103350844d48
Dan Delany <dand@spotify.com>
2017-05-07 20:44:10 -0400

    remove vertical flip

--- stream-udp.sh
+++ stream-udp.sh
@@ -1,2 +1,2 @@
-gst-launch-1.0 -e rpicamsrc vflip=true hflip=true bitrate=5000000 do-timestamp=true preview=false  ! 'video/x-h264,width=1024,height=768,framerate=30/1' ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink host=192.168.0.7 port=9000 sync=false async=true
+gst-launch-1.0 -e rpicamsrc hflip=true bitrate=5000000 do-timestamp=true preview=false  ! 'video/x-h264,width=1024,height=768,framerate=30/1' ! h264parse ! rtph264pay config-interval=1 pt=96 ! udpsink host=192.168.0.7 port=9000 sync=false async=true
 
